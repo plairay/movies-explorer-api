5311730fbe67ef868077d890b8fb5fcdc3506aaf
plairay <plairay@yandex.ru>
2021-09-22 11:23:46 +0300

    fixed auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -5,12 +5,9 @@ const { NODE_ENV, JWT_SECRET } = process.env;
 const { SECRET_CODE } = require('../utils/constants');
 
 module.exports = async (req, res, next) => {
-  console.log(req.cookies.jwt);
   try {
     const token = req.cookies.jwt;
     const payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : SECRET_CODE);
-    console.log(payload);
-    console.log(req.user);
     req.user = payload;
     return next();
   } catch (err) { return next(new UnAuthErr('Необходимо авторизоваться')); }
